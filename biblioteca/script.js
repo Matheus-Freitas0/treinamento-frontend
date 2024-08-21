a2b94438c0648eaf31b85057ea0495207e043aa2
MatheusFreitas <moncaomatheus@gmail.com>
2024-08-21 15:20:29 -0300

    library has been started

---
+++ biblioteca/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('form-livro');
+    const listaLivros = document.getElementById('lista-livros');
+    form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    const titulo = document.getElementById('titulo').value;
+    const autor = document.getElementById('autor').value;
+    const ano = document.getElementById('ano').value;
+    const livroItem = document.createElement('li');
+    livroItem.textContent = `${titulo} por ${autor} (${ano})`;
+    listaLivros.appendChild(livroItem);
+    form.reset(); 
+    });
+   });
+   
