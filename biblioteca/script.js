94b974cc65bff1a4f8792b0502b7a22a23a585fd
MatheusFreitas <moncaomatheus@gmail.com>
2024-08-21 15:32:44 -0300

    tweak js

--- biblioteca/script.js
+++ biblioteca/script.js
@@ -1,13 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
     const form = document.getElementById('form-livro');
+    
     const listaLivros = document.getElementById('lista-livros');
     form.addEventListener('submit', function(event) {
     event.preventDefault(); 
+    
     const titulo = document.getElementById('titulo').value;
+    
     const autor = document.getElementById('autor').value;
+    
     const ano = document.getElementById('ano').value;
+    
     const livroItem = document.createElement('li');
-    livroItem.textContent = `${titulo} por ${autor} (${ano})`;
+    
+    livroItem.textContent = `${titulo}  -  ${autor} (${ano})`;
     listaLivros.appendChild(livroItem);
     form.reset(); 
     });
